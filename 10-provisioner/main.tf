454874f2d0eedb2b35fe48498322c2b06005bb7f
Siva Kumar <sivakumar9399959803@gmail.com>
2023-03-05 11:59:03 +0530

    learn in terraform

---
+++ 10-provisioner/main.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-09d7039fc04d628ea"]
+  tags = {
+    Name = "demo"
+  }
+
+  provisioner "remote-exec" {
+
+    connection {
+      host     = self.public_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "echo Hello"
+    ]
+  }
+
+}
