1341c94140e92d6c26876fbf9bcf5976ecb56154
Siva Kumar <sivakumar9399959803@gmail.com>
2023-03-28 11:10:08 +0530

    learn terraform

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami = "ami-0a017d8ceb274537d"
+  ami = "ami-0089b8e98cd95257d"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-09d7039fc04d628ea"]
   tags = {
