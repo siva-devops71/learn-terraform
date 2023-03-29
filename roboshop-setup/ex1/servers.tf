43ca040fed154bf5686736cc0589cefb1f72977f
Siva Kumar <sivakumar9399959803@gmail.com>
2023-03-29 17:07:07 +0530

    learn terraform

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -1,11 +1,11 @@
-#data "aws_ami" "ami" {
- # most_recent = true
- # name_regex  = "Centos-8-DevOps-Practice"
- # owners      = ["973714476881"]
-#}
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
 
 resource "aws_instance" "frontend" {
-  ami = "ami-0089b8e98cd95257d"
+  ami =  "data.aws_ami.ami.image_id"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-09d7039fc04d628ea"]
   tags = {
