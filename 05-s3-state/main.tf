ce05708d614f08c95593c4df42cd90d4504c384e
Siva Kumar <sivakumar9399959803@gmail.com>
2023-03-31 11:07:59 +0530

    learn terraform

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-b71"
+    bucket = "terraform-c71"
     key    = "05-s3-state/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0a017d8ceb274537d"
+  ami                    = "ami-0089b8e98cd95257d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-09d7039fc04d628ea"]
   tags = {
